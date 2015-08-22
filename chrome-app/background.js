619b82c92b884ed4d08a002dd4bdc2f01bd3fd9d
Evan Xd <itoyxd@gmail.com>
2015-08-22 21:16:04 +0800

    Hide app window

--- chrome-app/background.js
+++ chrome-app/background.js
@@ -1,21 +1,11 @@
 'use strict';
 
-var APP_WIDTH = 350;
-var APP_HEIGHT = 100;
-
 chrome.app.runtime.onLaunched.addListener(function() {
   var appWindows = chrome.app.window.getAll();
   // Make sure launch index.html just one time.
   if (appWindows.length === 0) {
     chrome.app.window.create('index.html', {
-      outerBounds: {
-        left: screen.width - APP_WIDTH,
-        top: screen.height - APP_HEIGHT,
-        maxWidth: APP_WIDTH,
-        maxHeight: APP_HEIGHT,
-        minWidth: APP_WIDTH,
-        minHeight: APP_HEIGHT
-      }
+      hidden: true
     });
   }
 });
