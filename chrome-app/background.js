bd19222f22c968de4bc199f4a732d633519d2eb3
Evan Xd <itoyxd@gmail.com>
2015-08-22 21:08:43 +0800

    Make sure launch index.html just one time

--- chrome-app/background.js
+++ chrome-app/background.js
@@ -4,14 +4,18 @@ var APP_WIDTH = 350;
 var APP_HEIGHT = 100;
 
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    outerBounds: {
-      left: screen.width - APP_WIDTH,
-      top: screen.height - APP_HEIGHT,
-      maxWidth: APP_WIDTH,
-      maxHeight: APP_HEIGHT,
-      minWidth: APP_WIDTH,
-      minHeight: APP_HEIGHT
-    }
-  });
+  var appWindows = chrome.app.window.getAll();
+  // Make sure launch index.html just one time.
+  if (appWindows.length === 0) {
+    chrome.app.window.create('index.html', {
+      outerBounds: {
+        left: screen.width - APP_WIDTH,
+        top: screen.height - APP_HEIGHT,
+        maxWidth: APP_WIDTH,
+        maxHeight: APP_HEIGHT,
+        minWidth: APP_WIDTH,
+        minHeight: APP_HEIGHT
+      }
+    });
+  }
 });
